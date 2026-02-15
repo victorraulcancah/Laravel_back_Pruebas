3cbb188359d44544a4f6bda7f6e4ad9ae6a89207
victorraulcancah <vcanchari38@gmail.com>
2026-02-15 13:19:37 -0500

    n

--- App1/app/Models/Persona.php
+++ App1/app/Models/Persona.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Persona extends Model
 {
-    //
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'email',
+        'telefono',
+    ];
 }
