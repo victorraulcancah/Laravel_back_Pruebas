3d99f9a3a91aaffac6a5e4d725fa503128d0189b
victorraulcancah <vcanchari38@gmail.com>
2026-02-15 13:17:38 -0500

    inicio

---
+++ App1/app/Http/Controllers/PersonaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Persona $persona)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Persona $persona)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Persona $persona)
+    {
+        //
+    }
+}
